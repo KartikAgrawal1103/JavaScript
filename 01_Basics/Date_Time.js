0d9b38d10fb84bd2d26f427fc5aaef5d09bcf2f5
KartikAgrawal1103 <93371866+KartikAgrawal1103@users.noreply.github.com>
2024-08-02 13:37:14 +0000

    new

--- 01_Basics/Date_Time.js
+++ 01_Basics/Date_Time.js
@@ -18,13 +18,24 @@ let mCustomDate = new Date("2024-08-15") //way to declare date YYYY MM DD
 // 8/15/2024, 12:00:00 AM
 
 let timeStamp = Date.now()
-console.log(timeStamp)
-// console.log(timeStamp.toLocaleString())
-console.log(myCustomDate.getTime()) // mnore gunction arwe there to (get month+1) Plus 1 to get correct month , get day, get year etc EXPLORE
-console.log(Date.now())
-console.log(Math.floor(Date.now()/1000))
+// console.log(timeStamp)
+// // console.log(timeStamp.toLocaleString())
+// console.log(myCustomDate.getTime()) // mnore gunction arwe there to (get month+1) Plus 1 to get correct month , get day, get year etc EXPLORE
+// console.log(Date.now())
+// console.log(Math.floor(Date.now()/1000))
 
 // 1722604397533
 // 1719201600000
 // 1722604397538
 // 1722604397
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1)
+`${newDate.getDay()} and the time` //using Streing interpolartuion
+
+newDate.toLocaleString('Default',{
+    weekday:"long",
+})
+// yeh exactly same akaam akrega jo hyum ne dekhaa tha ki // console.log(mCustomDate.toLocaleString()); yeh jo krha rha tha 
+// bas aur thoda jyada custom foramt and better practicce k l iye use karte hai 
+
