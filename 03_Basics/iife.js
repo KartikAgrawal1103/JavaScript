676039c9f297fd8c3a11a080966f6f3ced280a49
KartikAgrawal1103 <93371866+KartikAgrawal1103@users.noreply.github.com>
2024-08-05 18:16:29 +0530

    Update iife.js

--- 03_Basics/iife.js
+++ 03_Basics/iife.js
@@ -18,3 +18,16 @@
 // do idffe ek saath likh diye  // dhyan de" " (); " do iffe ek sath likhne k liye yeh jaroroi h 
 // iffe with funct likh diye /
 // yeh sab interview questions hai dhyaan dena 
+
+//IFFE USAGE: 
+// Real-Life Example
+// Imagine you want to create a small application that tracks the number of times a 
+// button is clicked, but you don't want to expose the counter variable to the global scope.
+
+// An Immediately Invoked Function Expression (IIFE) is a function that is executed right after it
+// is defined. This is useful for creating a new scope and avoiding polluting the global namespace. 
+//  Here's a short and simple explanation along with a real-life example:
+
+// Definition
+// An IIFE is a function that is declared and immediately called. The syntax 
+// involves wrapping the function in parentheses and then invoking it right away:
